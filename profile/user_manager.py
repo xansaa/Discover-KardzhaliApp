ffe298b3ee2c113854e7d4b8079d91941c63aeee
Dimitar Markov <xansaa18@gmail.com>
2026-03-02 23:29:10 +0200

    Update user_manager.py

---
+++ profile/user_manager.py
@@ -0,0 +1,89 @@
+import json
+import os
+
+class UserManager:
+    
+    def __init__(self):
+        self.users_file = "data/users.json"
+        self.current_user = None
+        self._load_users()
+    
+    def _load_users(self):
+        if os.path.exists(self.users_file):
+            with open(self.users_file, 'r', encoding='utf-8') as f:
+                self.users = json.load(f)
+        else:
+            self.users = {}
+    
+    #Saves users to file
+    def _save_users(self):
+        os.makedirs(os.path.dirname(self.users_file), exist_ok=True)
+        with open(self.users_file, 'w', encoding='utf-8') as f:
+            json.dump(self.users, f, ensure_ascii=False, indent=2)
+    
+
+    #Register new user
+    def register(self, username, email, password):
+        if username in self.users:
+            return False, 
+        
+        if len(password) < 8:
+            return False, 
+        
+        self.users[username] = {
+            "email": email,
+            "password": password,  
+            "favorites": [],
+            "visited": []
+        }
+        self._save_users()
+        return True, 
+    
+    def login(self, username, password):
+        if username not in self.users:
+            return False, 
+        
+        if self.users[username]["password"] != password:
+            return False, 
+        
+        self.current_user = username
+        return True, 
+    
+    def logout(self):
+        self.current_user = None
+    
+    def is_logged_in(self):
+        return self.current_user is not None
+    
+    #Current user data
+    def get_current_user(self):
+        if self.current_user:
+            return {
+                "username": self.current_user,
+                "email": self.users[self.current_user]["email"],
+                "favorites": self.users[self.current_user].get("favorites", []),
+                "visited": self.users[self.current_user].get("visited", [])
+            }
+        return None
+    
+    #Add favorite place to user's list
+    def add_favorite(self, place_name):
+        if self.current_user:
+            if place_name not in self.users[self.current_user]["favorites"]:
+                self.users[self.current_user]["favorites"].append(place_name)
+                self._save_users()
+                return True
+        return False
+    
+    #Remove favorite place from user's list
+    def remove_favorite(self, place_name):
+        if self.current_user:
+            if place_name in self.users[self.current_user]["favorites"]:
+                self.users[self.current_user]["favorites"].remove(place_name)
+                self._save_users()
+                return True
+        return False
+
+
+# Singleton instance
+user_manager = UserManager()
